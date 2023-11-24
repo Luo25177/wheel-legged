f4b66826ad2fe7730b103e2cfd6227834acb3a54
C25177 <beloved25177@126.com>
2023-11-24 11:19:24 +0800

    11.24

---
+++ output/DefineID.h
@@ -0,0 +1,16 @@
+//创建画面begin的命令索引ID
+#define  _SCREEN_BEGIN                                                         0
+
+//画面begin的背景图片
+#define  _IMG_BEGIN                                                            0
+
+#define  _TXT_DIS__BEGIN_TEXT_DISPLAY1                                         1
+
+#define  _TXT_DIS__BEGIN_TEXT_DISPLAY2                                         2
+
+#define  _TXT_DIS__BEGIN_TEXT_DISPLAY3                                         3
+
+#define  _TXT_DIS__BEGIN_TEXT_DISPLAY4                                         4
+
+#define  _TXT_DIS__BEGIN_TEXT_DISPLAY5                                         5
+
